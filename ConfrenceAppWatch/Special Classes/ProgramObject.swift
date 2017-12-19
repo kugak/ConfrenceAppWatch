8739307637c1988825a58413d7db9f2c9398c041
Kugak <contact@kuga.ca>
2017-12-19 12:24:45 -0500

    added required view controllers and object for Program

---
+++ ConfrenceAppWatch/Special Classes/ProgramObject.swift
@@ -0,0 +1,13 @@
+//
+//  ProgramObject.swift
+//  ConfrenceAppWatch
+//
+//  Created by Kuga Kugaraja on 2017-12-19.
+//  Copyright © 2017 Kuga Kugaraja. All rights reserved.
+//
+
+import UIKit
+
+class ProgramObject: NSObject {
+
+}
---
+++ ConfrenceAppWatch/Special Classes/ProgramTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProgramTableViewCell.swift
+//  ConfrenceAppWatch
+//
+//  Created by Kuga Kugaraja on 2017-12-19.
+//  Copyright © 2017 Kuga Kugaraja. All rights reserved.
+//
+
+import UIKit
+
+class ProgramTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
