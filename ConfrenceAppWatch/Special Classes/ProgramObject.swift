0c731494833df84321603f22275b222d83ac983f
Kugak <contact@kuga.ca>
2017-12-19 12:39:07 -0500

    added initWIthData, encoder for Program Object

--- ConfrenceAppWatch/Special Classes/ProgramObject.swift
+++ ConfrenceAppWatch/Special Classes/ProgramObject.swift
@@ -2,12 +2,50 @@
 //  ProgramObject.swift
 //  ConfrenceAppWatch
 //
-//  Created by Kuga Kugaraja on 2017-12-19.
-//  Copyright © 2017 Kuga Kugaraja. All rights reserved.
+//  Handles data connectivity to and from the watch
 //
 
 import UIKit
 
-class ProgramObject: NSObject {
+class ProgramObject: NSObject, NSCoding {
+    
+    //define items that will be transffered
+    var title: String?
+    var speaker: String?
+    var from: String?
+    var to: String?
+    var details: String?
+    
+    //constructor to save data
+    func initWithData ( title: String,
+                        speaker: String,
+                        from: String,
+                        to: String,
+                        details: String)
+    {
+        self.title = title;
+        self.speaker = speaker;
+        self.from = from;
+        self.to = to
+        self.details = details
+    }
+    
+    //contructor for serializing and de-serializing object (NSCoding)
+    required convenience init? (coder decoder: NSCoder)
+    {
+        <#code#>
+    }
+    
+    //take object and send it to watch
+    func encode(with coder: NSCoder)
+    {
+        coder.encode(self.title, forKey: "title")
+        coder.encode(self.speaker, forKey: "speaker")
+        coder.encode(self.from, forKey: "from")
+        coder.encode(self.to, forKey: "to")
+        coder.encode(self.details, forKey: "details")
+
+
+    }
 
 }
