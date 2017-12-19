530d0dbadf5254e77256596c5326f05e18191d3f
Kugak <contact@kuga.ca>
2017-12-19 13:45:59 -0500

    added programRowController and Program View controller for Watch

---
+++ ConfrenceAppWatch WatchKit Extension/Interface Controller/ProgramInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  ProgramInterfaceController.swift
+//  ConfrenceAppWatch WatchKit Extension
+//
+//  Created by Kuga Kugaraja on 2017-12-19.
+//  Copyright © 2017 Kuga Kugaraja. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class ProgramInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ ConfrenceAppWatch WatchKit Extension/Special Classes/ProgRowController.swift
@@ -0,0 +1,13 @@
+//
+//  ProgRowController.swift
+//  ConfrenceAppWatch WatchKit Extension
+//
+//  Created by Kuga Kugaraja on 2017-12-19.
+//  Copyright © 2017 Kuga Kugaraja. All rights reserved.
+//
+
+import UIKit
+
+class ProgRowController: NSObject {
+
+}
