c992807faf1e3a1e8685b5d5e68c52bb9f8e5f19
Kugak <contact@kuga.ca>
2017-12-19 12:50:40 -0500

    added fake data to program view controller

--- ConfrenceAppWatch/View Controllers/ProgramViewController.swift
+++ ConfrenceAppWatch/View Controllers/ProgramViewController.swift
@@ -9,6 +9,52 @@
 import UIKit
 
 class ProgramViewController: UIViewController {
+    
+    var programs : [ProgramObject] = []
+    
+    func initFakeDetails()
+    {
+        let progObj = ProgramObject()
+            progObj.initWithData(
+            title: "Far and Away",
+            speaker: "Jim Kirk",
+            from: "Fri Oct 21, 3:15",
+            to: "4:00",
+            details: "Jim will Speak about something.")
+        
+        let progObj2 = ProgramObject()
+            progObj2.initWithData(
+            title: "Slow and Steady",
+            speaker: "Mr Spock",
+            from: "Fri Oct 31, 8:15",
+            to: "9:00",
+            details: "Mr Spock will Speak about something.")
+        
+        let progObj3 = ProgramObject()
+            progObj3.initWithData(
+            title: "Old and Boring",
+            speaker: "Mr Scott",
+            from: "Fri Nov 21, 3:15",
+            to: "4:00",
+            details: "Speaker will Speak about something.")
+        
+        let progObj4 = ProgramObject()
+        progObj4.initWithData(
+            title: "Another Talk",
+            speaker: "Mr Jium",
+            from: "Fri Dec 21, 3:15",
+            to: "4:00",
+            details: "Speaker will Speak about something.")
+        
+        //insert to array
+        programs.insert(progObj, at: 0)
+        programs.insert(progObj, at: 1)
+        programs.insert(progObj, at: 2)
+        programs.insert(progObj, at: 3)
+
+        
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
